eaa7cf093026b4128a1f97874a2d5727be539fd7
shubhendu-goel <shubhendu.goel@interviewbit.com>
2020-09-10 23:22:21 +0530

    Image validation added

--- instagram/app/models/post.rb
+++ instagram/app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ApplicationRecord
 	has_one_attached :image_file
-	validates :caption, presence: true, length: {minimum: 3} 
+	validates :caption, presence: true, length: {minimum: 3}
+	validate :image_validation
+	private
+		def image_validation
+			if image_file.attached? && !image_file.content_type.in?(%w(image/jpeg image/jpg image/png))
+				errors.add(:image_file , 'File must be an image with extension jpeg/jpg/png')
+			elsif !image_file.attached?
+			 	errors.add(:image_file , ' must be attached')
+
+			end
+		end 
 end
